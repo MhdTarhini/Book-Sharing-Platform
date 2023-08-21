7f4c2edcc283b2623553ffe93749c52f681ce8e6
MhdTarhini <mohamedtarhini95@gmail.com>
2023-08-21 16:23:33 +0300

    Frontend <> Create Login and Register Pages

---
+++ frontend/src/context/authContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [userData, setUserData] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = async (data) => {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/api/guest/login",
+      data
+    );
+    const userdata = await response.data;
+    setUserData(userdata.data);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+  const logout = async () => {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${userData.token}`;
+    await axios.post("http://127.0.0.1:8000/api/logout");
+    localStorage.clear();
+    setUserData(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(userData));
+  }, [userData]);
+
+  return (
+    <AuthContext.Provider value={{ userData, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
