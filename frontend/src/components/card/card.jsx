c7d1191be840855b16cfd36067cc0e0f677651cf
MhdTarhini <mohamedtarhini95@gmail.com>
2023-08-25 05:11:58 +0300

    Frontend <> Fix Styling

--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -131,8 +131,8 @@ function Card({ id, title, author, imageSrc, review, user, like, gener }) {
                   <path
                     d="M5 13.3636L8.03559 16.3204C8.42388 16.6986 9.04279 16.6986 9.43108 16.3204L19 7"
                     stroke="#000000"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                 </svg>
               ) : (
--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import "./navbar.css";
 import { AuthContext } from "../../context/authContext";
 import { useNavigate } from "react-router-dom";
@@ -6,15 +6,17 @@ import { useNavigate } from "react-router-dom";
 function Navbar({ onSearch }) {
   const navigate = useNavigate();
   const [searchText, setSearchText] = useState("");
-  const { logout } = useContext(AuthContext);
-  const handleSearch = () => {
-    onSearch(searchText);
-  };
-  handleSearch();
+  const { logout, userData } = useContext(AuthContext);
+
   const handleLogout = async () => {
     await logout();
     navigate("/");
   };
+
+  const handleSearch = () => {
+    onSearch(searchText);
+  };
+
   return (
     <div className="navbar">
       <div className="logo">Logo</div>
@@ -24,7 +26,8 @@ function Navbar({ onSearch }) {
           height="24px"
           viewBox="0 0 24 24"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={handleSearch}>
           <path
             d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
             stroke="#000000"
@@ -42,8 +45,11 @@ function Navbar({ onSearch }) {
           onChange={(e) => setSearchText(e.target.value)}
         />
       </div>
-      <div className="user-info" onClick={handleLogout}>
-        Logout
+      <div className="user-info">
+        <div>{`${userData.user.firstName} ${userData.user.lastName}`}</div>
+        <div className="logout" onClick={handleLogout}>
+          Logout
+        </div>
       </div>
     </div>
   );
